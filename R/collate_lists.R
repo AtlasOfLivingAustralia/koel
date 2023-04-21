ba70b4778ed2b0114917cc06882440217db00ed8
cjwaite23 <callumwaite2000@gmail.com>
2023-04-21 14:08:02 +0930

    clear up code (accidentally copied another script in)

--- R/collate_lists.R
+++ R/collate_lists.R
@@ -37,3 +37,32 @@ collate_lists <- function(path, list_suffix = "_list") {
 
   return(alerts_lookup)
 }
+
+#' Import all files, formatting with logical columns stating source
+#'
+#' Redoing Martin's function minus lapply and to hopefully run smoother
+#'
+#' @param df A `data.frame` containing columns 'path' and 'label'
+#' @importFrom readr read_csv
+#' @importFrom dplyr bind_rows
+#' @export
+
+get_species_lists <- function(df){
+  lapply(
+    split(df, seq_len(nrow(df))),
+    function(a){
+      x <- read_csv(a$path, show_col_types = FALSE)
+      ncol <- nrow(df)
+      y <- matrix(rep(FALSE, ncol),
+                  nrow = 1,
+                  ncol = ncol,
+                  dimnames = list(
+                    NULL,
+                    df$label)) |>
+        as.data.frame()
+      y[[a$label]] <- TRUE
+      bind_cols(x, y)
+    }
+  ) |>
+    bind_rows()
+}
