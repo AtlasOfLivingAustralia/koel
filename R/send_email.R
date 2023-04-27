a2770c374c9e9a6d731f2f212e87ccb55de9d508
cjwaite23 <callumwaite2000@gmail.com>
2023-04-27 15:18:10 +0930

    create and add functions to build and send emails

---
+++ R/send_email.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' @param recipients
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+send_email <- function(recipients){
+
+  email <- envelope() |>
+    from("biosecurity@ala.org.au") |>
+    to("biosecurity@ala.org.au ") |>
+    bcc(recipients) |>
+    subject("ALA biosecurity alert") |>
+    render("email_template.Rmd", include_css = "rmd")
+
+  smtp <- server(
+    host = "smtp-relay.gmail.com",
+    port = 587,
+    username = "biosecurity@ala.org.au",
+    password = "Pshz27HDhQJs3y"
+  )
+
+  smtp(email, verbose = TRUE)
+}
