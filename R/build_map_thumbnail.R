0be469bda352c12befdc004f4da7404bd5eb3e5d
cjwaite23 <callumwaite2000@gmail.com>
2023-04-28 12:18:59 +0930

    Working on email functions

--- R/build_map_thumbnail.R
+++ R/build_map_thumbnail.R
@@ -5,20 +5,20 @@
 #' @importFrom maptiles get_tiles
 #' @export
 
-map_plot <- function(a){
+build_map_thumbnail <- function(list_row){
   box_size <- 0.15
-  x <- a |> st_as_sf(
+  x <- list_row |> st_as_sf(
     coords = c("decimalLongitude", "decimalLatitude"),
     crs = "WGS84")
   x_box <- st_bbox(c(
-    xmin = a$decimalLongitude - box_size,
-    xmax = a$decimalLongitude + box_size,
-    ymin = a$decimalLatitude - box_size,
-    ymax = a$decimalLatitude + box_size),
+    xmin = list_row$decimalLongitude - box_size,
+    xmax = list_row$decimalLongitude + box_size,
+    ymin = list_row$decimalLatitude - box_size,
+    ymax = list_row$decimalLatitude + box_size),
     crs = "WGS84"
   )
   y <- get_tiles(x_box, zoom = 10, crop = TRUE)
-  png(filename = paste0("cache/maps/", a$recordID, ".png"))
+  png(filename = paste0("cache/maps/", list_row$recordID, ".png"))
   plot_tiles(y)
   plot(x, col = "black", cex = 5, pch = 16, add = TRUE) # errors here
   dev.off()
