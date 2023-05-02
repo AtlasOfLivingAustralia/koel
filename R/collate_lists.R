e3f3eb5982e801932d0f1eae45002f535a04bab0
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2023-05-03 09:49:11 +1000

    removes some comments

--- R/collate_lists.R
+++ R/collate_lists.R
@@ -16,24 +16,19 @@ collate_lists <- function(path, list_suffix = "_list") {
     stop("`path` and 'list_suffix` arguments must be character strings.")
   }
 
-  # add defensive programming for path name - must end in /
+  # path name must end in /
   if (substr(path, nchar(path), nchar(path)) != "/") {
     stop("Invalid path. Must be a character string ending in `/`")
   }
 
-  ### clean list names
-  # obtain file names from the directory
+  # clean list names
   file_names <- list.files(path)
-  # file paths
   file_paths <- paste0(path, file_names)
-  # labels remove the _list.csv
   labels <- gsub(paste0(list_suffix, ".csv"),
                  "",
                  file_names)
-  # clean the label names
   labels_lower <- tolower(labels)
 
-  ### create alerts_lookup
   # create alerts_lookup data.frame
   alerts_lookup <- data.frame(
     label = labels,
