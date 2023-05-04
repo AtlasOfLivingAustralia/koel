1842efdb9793a7c54175b2fb5c1f55f10be816b6
cjwaite23 <callumwaite2000@gmail.com>
2023-05-05 09:23:05 +0930

    Test files created. build_email edited to remove dependency on previously created objects. send_email edited to send message if no recipients present

--- R/build_email.R
+++ R/build_email.R
@@ -11,13 +11,36 @@
 
 build_email <- function(alerts_data, email_list, template_path) {
 
+  # defensive programming on inputs: alerts_data
+  if (!("data.frame" %in% class(alerts_data))) {
+    stop("`alerts_data` argument must be a data.frame or tibble")
+  } else if (!all(c("correct_name", "search_term") %in% colnames(alerts_data))) {
+    stop("`alerts_data` must at least have columns `correct_name` and `search_term`")
+  }
+  # defensive programming on inputs: email_list
+  if (!("data.frame" %in% class(common_names))) {
+    stop("`species_list` argument must be a data.frame or tibble")
+  } else if (!all(c("correct_name", "common_name") %in% colnames(common_names))) {
+    stop("`species_list` must at least have columns `correct_name` and `common_name`")
+  }
+  # defensive programming on inputs: path
+  if (!is.character(path) | substr(path, nchar(path), nchar(path)) != "/") {
+    stop("`path` argument but be a string ending in '/'")
+  } else if (!dir.exists(path)) {
+    stop("The directory specified by `path` does not exist")
+  }
+
   date_time <- Sys.time() |>
     gsub("\\s", "_", x = _) |>
     gsub(":", "-", x = _)
 
   if (nrow(alerts_data) > 0) {
 
-    purrr::map(.x = alerts_lookup$label,
+    # identify list names from alerts_data
+    list_names <- colnames(alerts_data)[(which(colnames(alerts_data) == "correct_name") + 1):
+                                          (which(colnames(alerts_data) == "common_name") - 1)]
+
+    purrr::map(.x = list_names,
                .f = function(list_name) {
                  list_col <- alerts_data[[list_name]]
                  if (any(list_col)) {
--- R/send_email.R
+++ R/send_email.R
@@ -9,19 +9,23 @@
 
 send_email <- function(recipients){
 
-  email <- envelope() |>
-    from("biosecurity@ala.org.au") |>
-    to("biosecurity@ala.org.au ") |>
-    bcc(recipients) |>
-    subject("ALA biosecurity alert") |>
-    render("email_template.Rmd", include_css = "rmd")
+  if (length(recipients) == 0) {
+    rlang::inform("No email recipients for this list. No email sent but the table html has been saved.")
+  } else {
+    email <- envelope() |>
+      from("biosecurity@ala.org.au") |>
+      to("biosecurity@ala.org.au ") |>
+      bcc(recipients) |>
+      subject("ALA biosecurity alert") |>
+      render("email_template.Rmd", include_css = "rmd")
 
-  smtp <- server(
-    host = "smtp-relay.gmail.com",
-    port = 587,
-    username = "biosecurity@ala.org.au",
-    password = "Pshz27HDhQJs3y"
-  )
+    smtp <- server(
+      host = "smtp-relay.gmail.com",
+      port = 587,
+      username = "biosecurity@ala.org.au",
+      password = "Pshz27HDhQJs3y"
+    )
 
-  smtp(email, verbose = TRUE)
+    smtp(email, verbose = TRUE)
+  }
 }
