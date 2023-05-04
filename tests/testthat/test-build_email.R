9f56e11068cfc8c50e0c9b57079015604b3ddd7c
cjwaite23 <callumwaite2000@gmail.com>
2023-05-05 09:23:05 +0930

    Test files created. build_email edited to remove dependency on previously created objects. send_email edited to send message if no recipients present

--- tests/testthat/test-build_email.R
+++ tests/testthat/test-build_email.R
@@ -1,6 +1,11 @@
+# testing for build_email()
+
 test_that("correct types of input are supplied", {
 
   # test class/type of alerts_data, email_list, template_path
+  # alerts_data must be a tibble with lots of columns
+  # email_list should be a csv but is currently loaded independently of the function
+  # template path should be a character string specifying the path to the template
 
 
 })
